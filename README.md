c2e39969dc269e85e517319b193be6a46ffc0cb4
Tuan-Anh Bui <tuananh.hufs@gmail.com>
2016-01-08 23:38:32 +0100

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # sinf_master_thesis
+
+Additional files for Master thesis of *Tuan-Anh BUI*, including:
+- Full installation scripts for local test-bed nd AWS test-bed (with Vagrant)
+- Raw experiment results
+- Miscellaneous scripts
---
+++ misc/iperf_iperf.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#sleep 12000
+hosts=("11.0.0.18" "11.0.0.21" "12.0.0.5" "12.0.0.6")
+for i in "${hosts[@]}"
+do
+	j="0"
+	while [ $j -lt 10 ]
+	do
+        	iperf -c $i -t 60 -i 1 >> "result_iperf_legacy_local_20160101.txt"
+		j=$[$j+1]
+    done
+done
+# external interface
+sleep 10
+hosts=("10.0.100.102" "10.0.100.103" "10.0.100.104" 
+		"10.0.100.105" "10.0.100.116" "10.0.100.117")
+#hosts=("10.0.100.116" "10.0.100.117")
+for i in "${hosts[@]}"
+do
+        j="0"
+        while [ $j -lt 10 ]
+        do
+                iperf -c $i -t 60 -i 1 >> "result_iperf_legacy_ext_20160101.txt"
+                j=$[$j+1]
+        done
+done
